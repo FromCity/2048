778861f9ee77e7755a2e8a516ad882587e22fbee
FromCity <AlekseiTetyuev@gmail.com>
2023-07-31 21:20:49 +0500

    del param, sts -> state

--- animation.js
+++ animation.js
@@ -1,27 +1,13 @@
 function animation(params) {
 	const { update, render, clear } = params;
-	let pTimestamp = 0;
 
 	requestAnimationFrame(tick);
 
-	function tick(timestamp) {
-		requestAnimationFrame(tick);
+	function tick() {
+		requestAnimationFrame(tick)
 
-		const diff = timestamp - pTimestamp;
-		const fps = 1000 / diff;
-		const secondPart = diff / 1000;
-		pTimestamp = timestamp;
-
-		const params = {
-			diff,
-			timestamp,
-			pTimestamp,
-			fps,
-			secondPart,
-		};
-
-		update(params)
+		update()
 		clear()
-		render(params)
+		render()
 	}
 }
